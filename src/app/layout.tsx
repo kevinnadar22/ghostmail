000450e2ee967cb92ebecfeac8732569dce9ecac
Kevin <jesikamaraj@gmail.com>
2026-02-15 15:51:58 +0530

    added vercel analystics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import { Providers } from "@/providers/query-provider";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -58,6 +59,7 @@ export default function RootLayout({
         <Providers>
           {children}
           <Toaster position="top-center" />
+          <Analytics />
         </Providers>
       </body>
     </html>
